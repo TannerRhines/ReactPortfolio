144d447eb3e3c295f084147424ee679536ba8cbb
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-15 02:01:36 -0800

    added portfolio page and reorganized code for routes in index.js, app.js

---
+++ react-portfolio/src/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h2>My Portfolio</h2>
+      <p>Project 1: </p>
+      <p>Project 2: </p>
+    </div>
+  );
+};
+
+export default Portfolio;
--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -2,25 +2,13 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AboutMe from './AboutMe';
-import Header from './Header'; 
-import NavBar from './NavBar';
-
-const Main = () => {
-  return (
-    <div>
-      <Header title="Tanner Rhines" />
-      <NavBar />
-      <AboutMe />
-      {/* {where other components will go} */}
-    </div>
-  );
-};
+import App from './App';  
 
 const root = document.getElementById('root');
 const reactRoot = createRoot(root);
+
 reactRoot.render(
   <React.StrictMode>
-    <Main />
+    <App />  
   </React.StrictMode>
 );
