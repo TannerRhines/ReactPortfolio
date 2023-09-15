65d4570f5dfd45245368be3c959e9f5cd0323279
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-15 01:41:48 -0800

    update error in console by using "react-dom/client" import

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,19 +1,22 @@
+
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header';  
+import { createRoot } from 'react-dom/client';
+import Header from './Header'; 
 
 const Main = () => {
   return (
     <div>
       <Header title="My App Title" />
-    
+      {/* {where other components will go} */}
     </div>
   );
 };
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const reactRoot = createRoot(root);
+reactRoot.render(
   <React.StrictMode>
     <Main />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
