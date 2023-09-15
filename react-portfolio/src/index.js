d0961bc3eda2d5818c3fae6b122ff1e4eee2966b
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-15 01:38:49 -0800

    created Header and populated with an app title, imported to app and index

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,5 +1,19 @@
-import hello from './hello';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';  
 
+const Main = () => {
+  return (
+    <div>
+      <Header title="My App Title" />
+    
+    </div>
+  );
+};
 
-
-hello();
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
