dc1803183eaf0d726c121fdb911fbae382a161bf
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-23 02:38:58 -0800

    added react-dom-client

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,14 +1,12 @@
-
-
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';  
+import { createRoot } from 'react-dom/client'; // Keep this line as is
+import App from './App';
 
 const root = document.getElementById('root');
 const reactRoot = createRoot(root);
 
 reactRoot.render(
   <React.StrictMode>
-    <App />  
+    <App />
   </React.StrictMode>
 );
