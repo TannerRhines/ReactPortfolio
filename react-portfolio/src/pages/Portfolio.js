8395c0f50ca17d41eff72789f0fe5a421fcdbf59
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-24 03:43:18 -0800

    added project function to portfolio.js

--- react-portfolio/src/pages/Portfolio.js
+++ react-portfolio/src/pages/Portfolio.js
@@ -1,3 +1,24 @@
+import React from 'react';
+import Project from './Project'; 
+
+
 export default function Portfolio() {
-    return <h1> portfolio </h1>
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <div className="projects-container">
+                <Project 
+                    imageSrc="path-to-your-project-image-1" 
+                    caption="Project 1" 
+                    projectLink=""
+                />
+                <Project 
+                    imageSrc="path-to-your-project-image-2" 
+                    caption="Project 2" 
+                    projectLink=""
+                />
+                {/* Add more Project components as needed */}
+            </div>
+        </div>
+    );
 }
