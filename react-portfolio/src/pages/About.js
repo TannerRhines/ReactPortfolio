e75f9e8e68b84e817ea81a313f154dc5e1bf430a
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-24 08:31:30 -0800

    added div styling to Avi on about page and changed footer settings

--- react-portfolio/src/pages/About.js
+++ react-portfolio/src/pages/About.js
@@ -1,13 +1,18 @@
 import world from '../assets/images/world.png'; 
 
+import avi from '../assets/images/tannerAvi.png'; 
+
 
 export default function About() {
     return (
         <div className='aboutMe'>
             <h1>about</h1>
-            <p> Hi world, I'm Tanner. </p>
+            <div style={{textAlign: 'center', margin: '20px 0'}}>
+            <img src={ avi } alt="pixelWorld" style={{maxWidth: '200px', maxHeight: '200px'}}></img>
+            </div>
+            <p> Hi world, my name is Tanner. </p>
 
-            <p> 🎓 I am currently a coding student through UC Berkeley Extension, gaining many career applicable skills in the world of tech. </p>
+            <p> 🎓 I am currently a coding student through UC Berkeley Extension, gaining many career applicable skills in the tech industry. </p>
 
 
             <p> 💻 When it comes to technology, I have a high level of interest in cloud computing. I have been working with Amazon Web Services, and have aquired lots of hands-on experience in deploying, managing, and maintaing highly available/ scalable systems. I am currently working on my next two AWS certifications. </p>
@@ -18,7 +23,7 @@ export default function About() {
             <p> 📍 I currently live in my hometown of Fairbanks, Alaska</p>
 
             <div style={{textAlign: 'center', margin: '20px 0'}}>
-                <img src={world} alt="Description" style={{maxWidth: '50px', maxHeight: '50px'}} />
+                <img src={world} alt="pixelWorld" style={{maxWidth: '50px', maxHeight: '50px'}} />
             </div>
         </div>
     );
