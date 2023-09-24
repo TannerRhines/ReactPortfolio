145ca5c9e83bbae21ef3540678982f13c49adcf0
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-24 06:04:55 -0800

    added padding to top of grid

--- react-portfolio/src/pages/Project.js
+++ react-portfolio/src/pages/Project.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Project({ imageSrc, altText, caption, projectLink }) {
   return (
-    <div className="col-12 col-sm-6 col-md-4">
+    <div className="col-12 col-sm-6 col-md-4 pt-5">
       <figure>
         <a className="d-block" href={projectLink}>
           <img src={imageSrc} alt={altText} className="img-fluid" />
