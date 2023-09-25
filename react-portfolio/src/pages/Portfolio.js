5f1de0a55f04e4a3dc8b37b6c7ede4475603b4b9
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-25 04:14:13 -0800

    added new projects to portfolio page

--- react-portfolio/src/pages/Portfolio.js
+++ react-portfolio/src/pages/Portfolio.js
@@ -5,14 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import eyeballs from '../assets/images/eyeballs.png';
 import runner from '../assets/images/runner.png';
 import artSite from '../assets/images/art.png';
+import tux from '../assets/images/tux.png';
+import mongo from '../assets/images/mongoDB.png';
+import weather from '../assets/images/weathermap.png';
 
 const projects = [
   { imageSrc: runner, altText: "runner", caption: "C++ Runner", projectLink: "https://github.com/TannerRhines/PixelRPG" },
   { imageSrc: eyeballs, altText: "interactive eyeballs", caption: "Interactive Eyeballs", projectLink: "https://tannerrhines.github.io/InteractiveEyeballs/" },
+  { imageSrc: tux, altText: "mac address changer", caption: "Mac Address Changer", projectLink: "https://github.com/TannerRhines/mac-address-changer" },
   { imageSrc: artSite, altText: "art website", caption: "Art Website", projectLink: "https://tannerrhines.com" },
-  { imageSrc: artSite, altText: "art website", caption: "Art Website", projectLink: "https://tannerrhines.com" },
-  { imageSrc: artSite, altText: "art website", caption: "Art Website", projectLink: "https://tannerrhines.com" },
-  { imageSrc: artSite, altText: "art website", caption: "Art Website", projectLink: "https://tannerrhines.com" }
+  { imageSrc: mongo, altText: "mongoDB", caption: "MongoDB BackEnd", projectLink: "https://github.com/TannerRhines/mongoDBsocial" },
+  { imageSrc: weather, altText: "weather api app", caption: "weather forecast app", projectLink: "https://github.com/TannerRhines/WeatherAPIApp1" }
 ];
 
 export default function Portfolio() {
